cb1252d626aad28d8d17c22993ccee3384f37a28
ISNing <isning_sf@outlook.com>
2025-11-16 17:42:22 +0800

    vscode: remove unnecessary Wayland command line arguments

--- home/linux/gui/base/wayland-apps.nix
+++ home/linux/gui/base/wayland-apps.nix
@@ -35,14 +35,6 @@
         isInsiders = false;
         # https://wiki.archlinux.org/title/Wayland#Electron
         commandLineArgs = [
-          "--ozone-platform-hint=auto"
-          "--ozone-platform=wayland"
-          # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
-          # (only supported by chromium/chrome at this time, not electron)
-          "--gtk-version=4"
-          # make it use text-input-v1, which works for kwin 5.27 and weston
-          "--enable-wayland-ime"
-
           "--password-store=gnome-libsecret" # use gnome-keyring as password store
         ];
       };
