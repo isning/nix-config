83db704d6b591a081e8e6fc5a7c71131936ddd26
ISNing <isning_sf@outlook.com>
2025-12-08 20:27:34 +0800

    feat(saika): re-enable binfmt emulation for aarch64 and riscv64

--- hosts/saika/hardware-configuration.nix
+++ hosts/saika/hardware-configuration.nix
@@ -39,13 +39,13 @@
   boot.tmp.cleanOnBoot = true;
 
   # Enable binfmt emulation of aarch64-linux, this is required for cross compilation.
-  #   boot.binfmt.emulatedSystems = [
-  #     "aarch64-linux"
-  #     "riscv64-linux"
-  #   ];
+  boot.binfmt.emulatedSystems = [
+    "aarch64-linux"
+    "riscv64-linux"
+  ];
   # This enables the kernel to preload the emulator binaries when the binfmt registrations are added,
   # obviating the need to make the emulator binaries available inside chroots and chroot-like sandboxes.
-  #   boot.binfmt.preferStaticEmulators = true; # required to work with podman
+  boot.binfmt.preferStaticEmulators = true; # required to work with podman
 
   # supported file systems, so we can mount any removable disks with these filesystems
   boot.supportedFilesystems = [
