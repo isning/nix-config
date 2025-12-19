31fd79ea0cb0af7893fd289535da192986584bae
Ryan Yin <ryan4yin@linux.com>
2025-12-19 09:56:46 +0800

    chore(home/base/tui/cloud): remove unused cloud tools

--- home/base/tui/cloud/default.nix
+++ home/base/tui/cloud/default.nix
@@ -20,24 +20,6 @@
     # pulumiPackages.pulumi-language-python
     # pulumiPackages.pulumi-language-nodejs
 
-    # aws
-    awscli2
-    ssm-session-manager-plugin # Amazon SSM Session Manager Plugin
-    aws-iam-authenticator
-    eksctl
-
-    # aliyun
-    aliyun-cli
-    # digitalocean
-    doctl
-    # google cloud
-    (google-cloud-sdk.withExtraComponents (
-      with google-cloud-sdk.components;
-      [
-        gke-gcloud-auth-plugin
-      ]
-    ))
-
     # cloud tools that nix do not have cache for.
     terraform
     terraformer # generate terraform configs from existing cloud resources
