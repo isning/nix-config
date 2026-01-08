35c481c979383fa974cc82f6020687318c4c08fb
ISNing <isning_sf@outlook.com>
2026-01-08 23:10:13 +0800

    refactor(fcitx5): restructure input method settings and remove profile configuration

--- home/linux/gui/base/fcitx5/default.nix
+++ home/linux/gui/base/fcitx5/default.nix
@@ -1,28 +1,70 @@
 { pkgs, ... }:
 {
-  xdg.configFile = {
-    "fcitx5/profile" = {
-      source = ./profile;
-      # every time fcitx5 switch input method, it will modify ~/.config/fcitx5/profile,
-      # so we need to force replace it in every rebuild to avoid file conflict.
-      force = true;
-    };
-  };
-
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.waylandFrontend = true;
-    fcitx5.addons = with pkgs; [
-      qt6Packages.fcitx5-configtool # GUI for fcitx5
-      fcitx5-gtk # gtk im module
 
-      # Chinese
-      fcitx5-rime # for flypy chinese input method
-      # fcitx5-chinese-addons # we use rime instead
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        qt6Packages.fcitx5-configtool # GUI for fcitx5
+        fcitx5-gtk # gtk im module
 
-      # Japanese
-      fcitx5-mozc-ut
-    ];
+        # Chinese
+        fcitx5-rime # for flypy chinese input method
+        # fcitx5-chinese-addons # we use rime instead
+
+        # Japanese
+        fcitx5-mozc-ut
+      ];
+      settings.inputMethod = {
+        "Groups/0" = {
+          "Name" = "Intl";
+          "Default Layout" = "us";
+          "DefaultIM" = "keyboard-us-altgr-intl";
+        };
+
+        "Groups/0/Items/0" = {
+          "Name" = "keyboard-us";
+          "Layout" = "";
+        };
+
+        "Groups/0/Items/1" = {
+          "Name" = "keyboard-us-intl";
+          "Layout" = "";
+        };
+
+        "Groups/0/Items/2" = {
+          "Name" = "keyboard-us-altgr-intl";
+          "Layout" = "";
+        };
+
+        "Groups/1" = {
+          "Name" = "Default";
+          "Default Layout" = "us";
+          "DefaultIM" = "rime";
+        };
+
+        "Groups/1/Items/0" = {
+          "Name" = "keyboard-us";
+          "Layout" = "";
+        };
+
+        "Groups/1/Items/1" = {
+          "Name" = "rime";
+          "Layout" = "";
+        };
+
+        "Groups/1/Items/2" = {
+          "Name" = "mozc";
+          "Layout" = "";
+        };
+
+        "GroupOrder" = {
+          "0" = "Default";
+          "1" = "Intl";
+        };
+      };
+    };
   };
 }
