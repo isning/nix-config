dce407fea02a49056a3054e0ed49c41ac25489ca
ISNing <isning_sf@outlook.com>
2026-02-23 17:34:54 +0800

    feat(autostart): remove alacritty, ghostty and firefox from autostart

--- home/linux/gui/base/xdg/autostart.nix
+++ home/linux/gui/base/xdg/autostart.nix
@@ -5,13 +5,10 @@
   # This fixes nixpak sandboxed apps (like firefox) accessing mapped folders correctly
   xdg.autostart.entries = [
     "${pkgs.foot}/share/applications/foot.desktop"
-    "${pkgs.alacritty}/share/applications/Alacritty.desktop"
-    "${pkgs.ghostty}/share/applications/com.mitchellh.ghostty.desktop"
 
     "${pkgs.clash-verge-rev}/share/applications/clash-verge.desktop"
 
     # nixpaks
-    "${pkgs.nixpaks.firefox}/share/applications/org.mozilla.firefox.desktop"
     "${pkgs.nixpaks.telegram-desktop}/share/applications/org.telegram.desktop.desktop"
   ]
   ++ (
