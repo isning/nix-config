198c1be4735f25879624ccdba35178893e4e0502
ISNing <isning_sf@outlook.com>
2026-03-01 09:05:26 +0800

    feat(tailscale): update extraSetFlags to include exit node and route advertisements

--- hosts/whitefox/tailscale.nix
+++ hosts/whitefox/tailscale.nix
@@ -1,6 +1,10 @@
 { config, ... }:
 {
   services.tailscale.enable = true;
-  services.tailscale.extraSetFlags = [ "--netfilter-mode=nodivert" ];
+  services.tailscale.extraSetFlags = [
+    "--netfilter-mode=nodivert"
+    "--advertise-exit-node"
+    "--advertise-routes=192.168.1.0/24,192.168.2.0/24,192.168.3.0/24,192.168.4.0/24"
+  ];
   services.tailscale.extraDaemonFlags = [ "--no-logs-no-support" ];
 }
